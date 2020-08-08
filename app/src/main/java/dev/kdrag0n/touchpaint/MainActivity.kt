18f449b76ab09b9a514d93a251f46da45a6420a6
Danny Lin <danny@kdrag0n.dev>
2020-08-07 22:24:53 -0700

    Initialize project with Android Studio

---
+++ app/src/main/java/dev/kdrag0n/touchpaint/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.kdrag0n.touchpaint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
